8d276e2691cf94eca9a8577107ac898bbb9517a7
AlexS9112 <Alexsultanov9112@gmail.com>
2023-02-11 17:34:21 +0300

    HWJAVA

---
+++ HW JS_functions.js
@@ -0,0 +1,42 @@
+// function f1(a1,a2){
+// console.log(a1 + a2)
+
+// }
+// f1(5,5)
+
+
+// function f2(a1,a2){
+//     console.log(a1 ** a2)
+
+// }
+// f2(5,2)
+
+
+
+// function f3(И,Ф,О){
+//      res = JSON.stringify({И, Ф, О})
+//      res1 = res.replace(/"([^"]+)":/g, '$1:')
+
+//      console.log(res1)
+//      return res1
+// }
+// f3 ("name","surname","middlename")
+
+
+function f4(q1){
+
+let w1 = []
+for (let i = 0; i < q1.length; i+= 1){
+ if (q1[i] % 2 == 0){
+    w1.push(q1[i])
+ }
+}
+if(w1.length){
+    return w1
+} else {
+    return 'No even numbers'
+}
+
+}
+let e1 = f4 [2,5,6,10,33,22,40]
+console.log(e1)
